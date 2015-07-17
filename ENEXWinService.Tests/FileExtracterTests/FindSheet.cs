943c55c6144e80e5de9681fa62ec2ebd015c36d9
jorge vazquez <vazquezromero@gmail.com>
2015-07-17 14:34:58 +0200

    Enex
    creating Test

---
+++ ENEXWinService.Tests/FileExtracterTests/FindSheet.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ENEXWinService.Model;
+using ENEXWinService.FileExtracter;
+using System;
+using ENEXWinService.Services;
+using ENEXWinService.Configuration;
+using ENEXWinService.Quartz;
+using Quartz;
+using Moq;
+using Quartz.Impl;
+using System.Text.RegularExpressions;
+using System.Data.OleDb;
+using System.Data;
+using System.Windows.Forms;
+using System.Globalization;
+
+
+namespace ENEXWinService.Tests.FileExtracterTests
+{
+    [TestClass]
+    public class FindSheet
+    {
+
+        [TestMethod]
+        [DeploymentItem(@"../../TestData/Production NALBANT Jan-Jul16 2015.xls", "TestData")]
+        public void FindMonth()
+        {
+
+            // initialize
+            OleDbConnection conn;
+            //OleDbDataAdapter adapter;
+            DataTable dt = null;
+
+            var file = @"TestData\Production NALBANT Jan-Jul16 2015.xls";
+
+            conn = new OleDbConnection("Provider=Microsoft.Jet.OleDb.4.0;" +
+                "Data Source=" + file + ";" +
+                "Extended Properties=Excel 8.0");
+            conn.Open();
+
+            // Get the data table containg the schema guid.
+            dt = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+            // End initialize
+
+            DateTime now = DateTime.Now;
+            string monthName = new DateTime(2010, 8, 1)
+                .ToString("MMM");
+            CultureInfo ru = new CultureInfo("ro-RO");
+            string currentMonth = now.ToString("MMM", ru);
+            currentMonth = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(currentMonth.Remove(currentMonth.Length - 1));
+
+            string getMonth = "";
+
+            if (dt != null)
+            {
+                String[] excelSheets = new String[dt.Rows.Count];
+                int i = 0;
+
+                // Add the sheet name to the string array.
+                foreach (DataRow row in dt.Rows)
+                {
+                    excelSheets[i] = row["TABLE_NAME"].ToString();
+                    if (excelSheets[i].Length < 13)
+                    {
+                        if (excelSheets[i].Substring(1,3) == currentMonth)
+                        {
+                            getMonth = excelSheets[i];
+                        }
+                    }
+                    i++;
+                }
+            }            
+
+            Assert.AreEqual(getMonth, "'Iul 2015$'");
+            
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"../../TestData/Production NALBANT Jan-Jul16 2015.xls", "TestData")]
+        public void FindYear()
+        {
+
+            // initialize
+            OleDbConnection conn;
+            //OleDbDataAdapter adapter;
+            DataTable dt = null;
+
+            var file = @"TestData\Production NALBANT Jan-Jul16 2015.xls";
+
+            conn = new OleDbConnection("Provider=Microsoft.Jet.OleDb.4.0;" +
+                "Data Source=" + file + ";" +
+                "Extended Properties=Excel 8.0");
+            conn.Open();
+
+            // Get the data table containg the schema guid.
+            dt = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+            // End initialize
+
+            DateTime now = DateTime.Now;
+
+            CultureInfo ru = new CultureInfo("ro-RO");
+            int currentYear = now.Year;
+            int getYear = 0;
+
+            if (dt != null)
+            {
+                String[] excelSheets = new String[dt.Rows.Count];
+                int i = 0;
+
+                // Add the sheet name to the string array.
+                foreach (DataRow row in dt.Rows)
+                {
+                    excelSheets[i] = row["TABLE_NAME"].ToString();
+                    // if (excelSheets[i] == rumMonth) break;
+                    if (excelSheets[i].Length < 13)
+                    {
+                        string getDate = excelSheets[i].Substring(5,4);
+                        if (Convert.ToInt32(getDate) == currentYear)
+                        {
+                            getYear = Convert.ToInt32(excelSheets[i].Substring(5, 4));
+                        }
+                    }
+                    i++;
+                }
+            }
+
+
+            Assert.AreEqual(getYear, 2015);
+
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"../../TestData/Production NALBANT Jan-Jul16 2015.xls", "TestData")]
+        public void FindDateFirstLine()
+        {
+
+            // initialize
+            OleDbConnection conn;
+            //OleDbDataAdapter adapter;
+            DataTable dt = null;
+            DataSet ds = new DataSet();
+            
+            var file = @"TestData\Production NALBANT Jan-Jul16 2015.xls";
+
+            conn = new OleDbConnection("Provider=Microsoft.Jet.OleDb.4.0;" +
+                "Data Source=" + file + ";" +
+                "Extended Properties=Excel 8.0");
+            conn.Open();
+            OleDbCommand cmd = new OleDbCommand();
+            cmd.Connection = conn;
+
+            // Get the data table containg the schema guid.
+            // dt = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+            // End initialize
+            
+            string sheetName = "'Iul 2015'";
+            cmd.CommandText = "SELECT * FROM [" + sheetName + "]";
+
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            dt.TableName = sheetName;
+
+            da.Fill(dt);
+            ds.Tables.Add(dt);
+
+
+
+
+         //   Assert.AreEqual(getYear, 2015);
+
+        }
+
+    
+    }
+
+
+}
